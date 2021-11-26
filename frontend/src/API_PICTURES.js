cc836b3e7427cebebf8c3ff39911093a2e0d7d4b
MatthieuBRB <matthieu.barbeau2000@gmail.com>
2021-11-26 18:53:42 +0100

    Planets list - prices + names

--- frontend/src/API_PICTURES.js
+++ frontend/src/API_PICTURES.js
@@ -1,31 +1,29 @@
 import axios from "axios";
 
-const idList = ["PIA01492", "PIA03456", "PIA00583", "PIA01536", "PIA04591", "PIA00271"];
-
 export default class API_PICTURES {
-    fetchPlanetPicture(choice) {
+    fetchPlanetPicture(choice, idList) {
         var id;
         switch (choice) {
             case 0:
-                id = idList[0];
+                id = Object.values(idList)[0];
                 break;
             case 1:
-                id = idList[1];
+                id = Object.values(idList)[1];
                 break;
             case 2:
-                id = idList[2];
+                id = Object.values(idList)[2];
                 break;
             case 3:
-                id = idList[3];
+                id = Object.values(idList)[3];
                 break;
             case 4:
-                id = idList[4];
+                id = Object.values(idList)[4];
                 break;
             case 5:
-                id = idList[5];
+                id = Object.values(idList)[5];
                 break;
             default:
-                id = idList[0];
+                id = Object.values(idList)[6];
                 break;
         }
 
--- frontend/src/Planet.js
+++ frontend/src/Planet.js
@@ -2,17 +2,31 @@ import React from 'react';
 import API_PICTURES from './API_PICTURES.js';
 
 const apiPictures = new API_PICTURES();
+const idList = {
+    "Mars": "PIA04591",
+    "Venus": "PIA00271",
+    "Io": "PIA00583",
+    "Triton": "PIA01536",
+    "Callisto": "PIA03456",
+    "Neptune": "PIA01492"
+};
+const priceList = ["159,999,999,999.99$", "2,099,000,000,000.99$", "585,000,000,000.00$", "320,000,000,000.99$", "694,500,000,000.00$", "215,999,999,999.99$"];
+
 
 export default class Planet extends React.Component {
     getData() {
         var choice = this.props.choice;
         apiPictures
-            .fetchPlanetPicture(choice)
+            .fetchPlanetPicture(choice, idList)
             .then(function (response) {
                 var data = response.data;
                 var src = data.collection.items[0].href;
 
-                document.getElementsByClassName("planet-item")[choice].innerHTML = `<img src=${src} alt="planet" width="350px" height="350px"></img>`;
+                document.getElementsByClassName("planet-picture")[choice].innerHTML = `<img src=${src} alt="planet" width="350px" height="350px"></img>`;
+
+                var price = Math.floor(Math.random() * 6);
+                document.getElementsByClassName("planet-price")[choice].innerHTML = priceList[price];
+                document.getElementsByClassName("planet-name")[choice].innerHTML = Object.keys(idList)[choice];
             })
             .catch(function (error) {
                 console.error(error);
@@ -22,7 +36,11 @@ export default class Planet extends React.Component {
     render() {
         this.getData();
         return (
-            <div className="planet-item col-auto"></div>
+            <div className="col-auto">
+                <div className="planet-picture"></div>
+                <div className="planet-name"></div>
+                <div className="planet-price"></div>
+            </div>
         );
     }
 }

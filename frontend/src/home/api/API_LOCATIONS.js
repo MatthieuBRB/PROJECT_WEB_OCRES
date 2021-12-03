8960c72cacb01c8af128d844811db8e663c13198
MatthieuBRB <matthieu.barbeau2000@gmail.com>
2021-12-03 11:58:00 +0100

    Delete home folder

--- frontend/src/home/api/API_LOCATIONS.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default class API_LOCATIONS {
-    fetchLocationsStarWars() {
-        return axios
-        .get("http://localhost:3000/locations", {crossdomain: true})
-    }
-}
--- frontend/src/home/api/API_PERSON.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default class API_PERSON {
-    fetchPersonInSpace() {
-        return axios
-        .get("http://api.open-notify.org/astros.json", {crossdomain: true})
-    }
-}

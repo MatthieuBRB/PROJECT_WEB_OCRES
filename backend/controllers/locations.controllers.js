f150991bc031780f66b9cb98ab09aa52a47d34bc
MatthieuBRB <matthieu.barbeau2000@gmail.com>
2021-12-04 12:20:28 +0100

    Find by name, delete, create

--- backend/controllers/locations.controllers.js
+++ backend/controllers/locations.controllers.js
@@ -15,4 +15,55 @@ function findAll(req, res) {
         })
 }
 
-module.exports = { findAll }
+function findByName(req, res) {
+    const nameParam = req.params.name;
+    return Location.find({name: nameParam})
+        .exec()
+        .then((result) => {
+            if (result.length > 0) {
+                res.json(result)
+            } else {
+                res.status(202).json({ message: 'no locations available' })
+            }
+        })
+        .catch((err) => {
+            res.status(500).json(err)
+        })
+}
+
+function saveOne(req, res) {
+    const newLocation = new Location(req.body);
+
+    return newLocation
+        .save()
+        .then((result) => {
+            res
+                .status(201)
+                .json({ message:  `user ${result.id} created`, content: result })
+        })
+        .catch((err) => {
+            if(err.errors && Object.keys(err.errors).length > 0 && err.name === 'ValidationError') {
+                res.status(422).json({ message: err.message })
+            } else {
+                res.status(500).json(err)
+            }
+        })
+}
+
+function deleteByName(req, res) {
+    const nameParam = req.params.name;
+
+    return Location.deleteOne({ name: nameParam })
+        .then((result) => {
+            if(result) {
+                res.json({ message: `${result.deletedCount} deleted` })
+            } else {
+                res.status(404).json({ message: `Location not found`})
+            }
+        })
+        .catch((err) => {
+            res.status(500).json(err)
+        })
+}
+
+module.exports = { saveOne, findAll, findByName, deleteByName }
--- backend/routes/locations.routes.js
+++ backend/routes/locations.routes.js
@@ -3,5 +3,10 @@ var router = express.Router();
 const Location = require('../controllers/locations.controllers.js');
 
 router.get('/', Location.findAll);
+router.get('/:name', Location.findByName);
+
+router.post('/', Location.saveOne);
+
+router.delete('/:name', Location.deleteByName);
 
 module.exports = router;
